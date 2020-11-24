c5edd2a1b711dd2558607cc6ac4062daeaa03bb6
Belayet-aaroza <belayet.aaroza@gmail.com>
2020-11-24 15:26:13 +0600

    add test cases

--- tests/students.test.js
+++ tests/students.test.js
@@ -2,21 +2,55 @@ import { Meteor } from 'meteor/meteor';
 import { Students } from '../imports/collections/students';
 import { assert } from 'chai';
 
+let studentId;
 if (Meteor.isServer) {
-    describe('Student', () => {
+    describe('Students test cases', () => {
         describe('methods', () => {
             beforeEach(() => {
                 Students.remove({});
-                // studentId = Students.insert({
-                //     dob: "1994-01-26",
-                //     name: "Belayet",
-                //     email: "belayet@gmail.com",
-                //     phone: "12345677890",
-                //     subjects: ['Bangla', 'Math'],
-                //     createdAt: new Date(),
-                // });
             });
             it('can delete owned student', () => {
+                assert.equal(Students.find().count(), 0);
+            });
+        });
+
+        describe('insert subject', () => {
+            beforeEach(() => {
+                studentId = Students.insert({
+                    dob: "1994-01-26",
+                    name: "Belayet",
+                    email: "belayet@gmail.com",
+                    phone: "12345677890",
+                    Students: ['Bangla', 'Math'],
+                    createdAt: new Date(),
+                });
+            });
+            it('can create subject', () => {
+                assert.equal(Students.find().count(), 1);
+            });
+        });
+        describe('update subject', () => {
+            beforeEach(() => {
+                Students.update(studentId, {
+                    $set: {
+                        dob: "1994-01-26",
+                        name: "Belayet Hossain",
+                        email: "belayet@gmail.com",
+                        phone: "12345677890",
+                        Students: ['Bangla', 'Math', 'English'],
+                        updatedAt: new Date(),
+                    },
+                });
+            });
+            it('can update subject', () => {
+                assert.equal(Students.find().count(), 1);
+            });
+        });
+        describe('delete subject', () => {
+            beforeEach(() => {
+                Students.remove(studentId);
+            });
+            it('can delete subject', () => {
                 assert.equal(Students.find().count(), 0);
             });
         });
--- tests/subjects.test.js
+++ tests/subjects.test.js
@@ -2,13 +2,48 @@ import { Meteor } from 'meteor/meteor';
 import { Subjects } from '../imports/collections/subjects';
 import { assert } from 'chai';
 
+let subjectId;
 if (Meteor.isServer) {
-    describe('Student', () => {
-        describe('methods', () => {
+    describe('Subjects test cases', () => {
+        describe('delete all subjects', () => {
             beforeEach(() => {
                 Subjects.remove({});
             });
-            it('can delete owned student', () => {
+            it('can delete owned subject', () => {
+                assert.equal(Subjects.find().count(), 0);
+            });
+        });
+        describe('insert subject', () => {
+            beforeEach(() => {
+                subjectId = Subjects.insert({
+                    name: 'Bangla',
+                    students: ['belayet', 'asik'],
+                    createdAt: new Date(),
+                });
+            });
+            it('can create subject', () => {
+                assert.equal(Subjects.find().count(), 1);
+            });
+        });
+        describe('update subject', () => {
+            beforeEach(() => {
+                Subjects.update(subjectId, {
+                    $set: {
+                        name: 'Bangla',
+                        students: ['belayet', 'asif'],
+                        updatedAt: new Date(),
+                    },
+                  });
+            });
+            it('can update subject', () => {
+                assert.equal(Subjects.find().count(), 1);
+            });
+        });
+        describe('delete subject', () => {
+            beforeEach(() => {
+                Subjects.remove(subjectId);
+            });
+            it('can delete subject', () => {
                 assert.equal(Subjects.find().count(), 0);
             });
         });
